a83f833cd19b071884c5e6a11e7549f4a328ab26
mohamadlaqees <mohamadlk24@gmail.com>
2024-03-16 00:29:55 +0300

    complete error page

--- project/src/pages/Error.jsx
+++ project/src/pages/Error.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-import '../index.css'
+import "../index.css";
+import { useRouteError } from "react-router-dom";
 const Error = () => {
+  const error = useRouteError();
   return (
-    <div className='error'> 
-      <h1>{'error'}</h1>
+    <div className="error">
+      <h1>error</h1>
+      <p className="text">Invalid '{error.value}'</p>
     </div>
   );
 };
